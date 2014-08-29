f3a80fff030d5a4ac94ddfb8716e5a2d92cd8d4c
Juanma <juanmanuel.rodulfo@beeva.com>
2014-08-30 00:11:04 +0200

    updated hierarchy

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetaBlog::Application.config.secret_token = 'd67a22cb34d16191267f234bc761f35e88b3a2026d400d9f7390b24dc83f4f2a34c1b39938e55f67368f4b0fb767f7c36c27482a2e8ebaffef8885ff25b59227'
